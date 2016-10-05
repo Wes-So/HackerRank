c66301cbe7eb8ca68a4fbb10d0be90abc8d63898
wso <wso@CA-50-0963.corp.collectamerica.com>
2016-10-04 18:24:54 -0600

    Added solution for left hand rotation

--- HackerRank/src/CrackTheCode/LeftHandRotation.java
+++ HackerRank/src/CrackTheCode/LeftHandRotation.java
@@ -1,5 +1,40 @@
 package CrackTheCode;
 
+import java.util.ArrayList;
+
 public class LeftHandRotation {
 
+	   public static int[] arrayLeftRotation(int[] a, int n, int k) {
+	    	
+	    	ArrayList<Integer> arList = convertToArrayList(a,n); 
+	    	
+	    	
+	    	
+	    	for(int i = 0; i < k; i++){
+	        	int rem = arList.remove(0);
+	        	arList.add(rem);
+	    	}
+	    	 
+	    	
+	      return convertToIntArray(arList,n) ;
+	    }
+	    
+	    private static int[] convertToIntArray(ArrayList<Integer> arList, int n){
+	    	int[] a = new int[n];
+	    	for(int i = 0; i < n; i++){
+	    		a[i] = arList.get(i);
+	    	}
+	    	
+	    	return a;
+	    }
+	    
+	    private static ArrayList<Integer> convertToArrayList(int[] a, int n){
+	    	ArrayList<Integer> arList = new ArrayList<Integer>();
+	    	for(int i = 0; i < n; i++){
+	    		arList.add(a[i]);	
+	    	}
+	    	 
+	    	return arList;
+	    }
+	
 }
